9dcbb1141a4d039dbd0d6d81bcf26ef2b3b5d24e
Dheivv <davidecatozzi0@gmail.com>
2025-02-10 03:41:59 +0100

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError.js');
 const globalErrorHandler = require('./controllers/errorController.js');
@@ -28,6 +29,11 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS (cross origin resource sharing)
+// Allow access to the API to anyone
+app.use(cors());
+app.options('*', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
