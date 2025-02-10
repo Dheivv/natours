928a50f4abeea97f58365f1e38e32e51285bf6cf
Dheivv <davidecatozzi0@gmail.com>
2025-02-10 02:52:29 +0100

    Adjusted for secure connections

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const viewRouter = require('./routes/viewRoutes.js');
 // Start express app
 const app = express();
 
+// Enable proxies
+app.enable('trust proxy');
+
 // Set default view engine
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
