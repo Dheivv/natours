f87fbf35d7c7fa8537437b7f7887b7c3c1970611
Dheivv <davidecatozzi0@gmail.com>
2025-02-10 15:24:54 +0100

    fixed bug that displayed alert box in every page

--- public/js/index.js
+++ public/js/index.js
@@ -73,4 +73,4 @@ if (bookBtn) {
 }
 
 const alertMessage = document.querySelector('body').dataset.alert;
-if (alertMessage) showAlert('success', alertMessage, 20);
+if (alertMessage && alertMessage != '') showAlert('success', alertMessage, 20);
