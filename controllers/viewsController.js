aa427f9f470fc60da6d0ebe54d7bce47aee44684
Dheivv <davidecatozzi0@gmail.com>
2025-02-13 03:23:48 +0100

    further adjusted contacts page; book tour button now keeps track of past bookings and their dates

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -3,6 +3,7 @@ const User = require('../models/userModel.js');
 const Booking = require('../models/bookingModel.js');
 const catchAsync = require('../utils/catchAsync.js');
 const AppError = require('../utils/appError.js');
+const authController = require('./authController.js');
 
 exports.alerts = (req, res, next) => {
   const { alert } = req.query;
@@ -31,8 +32,17 @@ exports.getTour = catchAsync(async (req, res, next) => {
     fields: 'review rating user',
   });
 
+  const booking = await Booking.find({ price: tour.price });
+
   if (!tour) {
     return next(new AppError('There is no tour with that name!', 404));
+  }
+
+  const too_late = [];
+  for (let i = 0; i <= tour.startDates.length - 1; i++) {
+    too_late.push(
+      Date.now() / 1000 > parseInt(tour.startDates[i].getTime() / 1000, 10),
+    );
   }
 
   // 2) Build template
@@ -42,6 +52,8 @@ exports.getTour = catchAsync(async (req, res, next) => {
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
     tour: tour,
+    booking: booking,
+    too_late: too_late,
   });
 });
 
